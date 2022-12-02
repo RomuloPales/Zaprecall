c850e633d7d0005e4191b8b194aa58fd8237c6cd
RomuloPales <romulopales15@gmail.com>
2022-12-02 17:15:51 -0300

    layout finalizado

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -11,10 +11,13 @@ export default function Deck() {
     <Flashcards />
     <Flashcards />
     <Flashcards />
-    <Flashcards />
-    <Flashcards />
-    
-    <Footer>2/4 Concluidos</Footer>
+    <BotaoFooter>
+        <BotãoEscolha> não lembrei</BotãoEscolha>
+        <BotãoEscolha>Quase não lembrei</BotãoEscolha>
+        <BotãoEscolha>Zap!</BotãoEscolha>
+
+    </BotaoFooter>
+    <Footer >2/4 Concluidos</Footer>
     </>
   )
 }
@@ -35,4 +38,29 @@ const Footer = styled.div `
   font-size: 18px;
   color: #333333;
   padding: 10px;
+`
+const BotaoFooter = styled.div `
+
+  display: flex;
+  width: 80%;
+  justify-content: space-between;
+  margin: 20px;
+`
+
+const BotãoEscolha = styled.div `
+    width: 90px;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: #FFFFFF;
+    background: blue;
+    border-radius: 5px;
+    border: 1px solid blue;
+    padding:5px;
 `
